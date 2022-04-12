cc16231d8c39060af429f760efe6f3f7b8b779c6
Yuri <arhipow.yuri@gmail.com>
2022-04-13 03:18:30 +0600

    made charList and some more

--- src/components/services/MarvelService.js
+++ src/components/services/MarvelService.js
@@ -12,7 +12,7 @@ class MarvelService {
     }
 
     getAllCharacters = async () => {
-        const res= await this.getResource(` ${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
@@ -23,6 +23,7 @@ class MarvelService {
 
     _transformCharacter = (char) => {
         return{
+            id: char.id,
             name: char.name, 
             description: char.description ? `${char.description.slice(0, 210)}...` : 'пуста',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
