2a12ad213cff5dd676d95a7d0ae068a4eada0940
Юрий Архипов <arhipow.yuri@gmail.com>
2022-06-08 19:52:22 +0600

    make my hooks

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-import MarvelService from '../services/MarvelService';
+import useMarvelService from '../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Skeleton from '../skeleton/Skeleton'
@@ -11,25 +11,13 @@ import './charInfo.scss';
 
 const CharInfo = (props) => {
     const [char, setChar] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState (false);
+    
 
 
-    const marvelService = new MarvelService();
+    const {loading, error, getCharacter} = useMarvelService();
 
     const onCharLoaded = (char) => {
         setChar(char)
-        setLoading(false)
-    }
-
-    const onCharLoading = () => {
-        setLoading(true)
-    }
-
-    const onError = () => {
-        setLoading(false);
-        setError(true);
-        
     }
     
     useEffect(() => {
@@ -37,28 +25,15 @@ const CharInfo = (props) => {
 
     }, [props.charId])
 
-    // componentDidMount() {
-    //     this.updateChar()
-    // }
-
-    // componentDidUpdate(prevProps) {
-    //     if (this.props.charId !== prevProps.charId) {
-    //         this.updateChar()
-    //     }
-    // }
-
-    
     const updateChar = () => {
         const {charId} = props;
         if (!charId) {
             return
         }
 
-        onCharLoading()
-        marvelService
-            .getCharacter(charId)
+        getCharacter(charId)
             .then(onCharLoaded)
-            .catch(onError)
+
     }
     
     const skeleton = char || loading || error ? null : <Skeleton/>;
